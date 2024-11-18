6331b8be1ea4e03d4ec31ce9eb652c6fecb6c39f
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-11-18 10:00:55 +0900

    fix

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -1,5 +1,5 @@
 --
 DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
-UPDATE id_generator SET id=2678400000 WHERE stub='a';
+TRUNCATE TABLE id_generator;
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
